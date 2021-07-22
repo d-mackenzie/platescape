a7a1b9281adf1172036b9e27f2a1af09c4568c21
David Mackenzie <david@priz.co.uk>
2021-07-22 21:42:45 +0100

    Add buttons to splash screen.

--- Chantry.App/ViewModels/SplashViewModel.cs
+++ Chantry.App/ViewModels/SplashViewModel.cs
@@ -11,19 +11,5 @@ namespace TeethInc.Chantry.App.ViewModels
 {
     public class SplashViewModel : BaseViewModel
     {
-        public Bitmap BackgroundBitmap
-        {
-            get
-            {
-                var assembly = Assembly.GetExecutingAssembly();
-                var resourceName = "TeethInc.Chantry.App.Assets.splash.jpg";
-
-                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-                {
-                    return new Bitmap(stream);
-
-                }
-            }
-        }
     }
 }
