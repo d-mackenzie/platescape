f703835242841c7ee5fb963d99a11e24da064c69
David Mackenzie <david@priz.co.uk>
2021-08-26 21:39:14 +0100

    Optimize mosaic renderer by rendering geometry path.

--- Chantry/UserControls/MosaicRenderer.axaml.cs
+++ Chantry/UserControls/MosaicRenderer.axaml.cs
@@ -2,7 +2,9 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using TeethInc.Chantry.Core.Services;
 
 namespace TeethInc.Chantry.App.UserControls
@@ -39,17 +41,37 @@ namespace TeethInc.Chantry.App.UserControls
 
             var colors = Mosaic.Colors;
 
+            // var colorDict = new Dictionary<Color, StringBuilder>();
+
+            //for (int x = 0; x < colors.GetUpperBound(0); x++)
+            //{
+            //    for (int y = 0; y < colors.GetUpperBound(1); y++)
+            //    {
+            //        Color color = Color.FromRgb(colors[x, y].Color.R, colors[x, y].Color.G, colors[x, y].Color.B);
+            //        context.DrawRectangle(
+            //            new SolidColorBrush(color),
+            //            new Pen(new SolidColorBrush(color)),
+            //            new Rect(x * 10, y * 10, 10, 10));
+            //    }
+            //}
+
+            var sb = new StringBuilder();
+
             for (int x = 0; x < colors.GetUpperBound(0); x++)
             {
                 for (int y = 0; y < colors.GetUpperBound(1); y++)
                 {
-                    Color color = Color.FromRgb(colors[x, y].Color.R, colors[x, y].Color.G, colors[x, y].Color.B);
-                    context.DrawRectangle(
-                        new SolidColorBrush(color),
-                        new Pen(new SolidColorBrush(color)),
-                        new Rect(x * 10, y * 10, 10, 10));
+                    sb.Append($"M {x * 10} {y * 10} l 10,0 0,10 -10,0 Z ");
                 }
             }
+
+            GeometryDrawing gd = new GeometryDrawing();
+            gd.Geometry = Geometry.Parse(sb.ToString());
+
+            gd.Pen = new Pen(Brushes.Brown);
+            gd.Brush = Brushes.Brown;
+
+            gd.Draw(context);
 
             Debug.WriteLine($"Render(): {sw.ElapsedMilliseconds}ms");
 
