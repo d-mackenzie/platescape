fa6345f98624ee339d7623f3f39a70508007f6d7
David Mackenzie <david@priz.co.uk>
2024-01-20 21:42:21 +0000

    Refactor MosaicService to use refactored Algorithm classes

--- Chantry.Core/Services/MosaicService.cs
+++ Chantry.Core/Services/MosaicService.cs
@@ -22,34 +22,25 @@ namespace TeethInc.Chantry.Core.Services
 
 		public Mosaic GetMosaic(SKBitmap filteredImage, ExtentSettings extentSettings, AlgorithmSettings algorithmSettings)
 		{
-			_colorService.AllowedColors = algorithmSettings.AllowedColors;
-
 			var sw = Stopwatch.StartNew();
 
 			SKSizeI elementExtent = extentSettings.ElementExtent;
 
 			var colors = new LdColor[elementExtent.Width, elementExtent.Height];
-			algorithmSettings.Algorithm.Reset(extentSettings.ElementExtent);
 
 			using (SKBitmap source = filteredImage.Resize(filteredImage.Info.Size.GetSizeToFill(elementExtent), SKFilterQuality.High))
 			{
 				SKBitmap mosaic = new SKBitmap(elementExtent.Width, elementExtent.Height);
 
-				SKPointI topLeft = new SKPointI(
-					(source.Width - mosaic.Width) / 2,
-					(source.Height - mosaic.Height) / 2);
-
-				var sourcePixels = source.Pixels;
 				var mosaicPixels = mosaic.Pixels;
+
+				var mosaicColors = algorithmSettings.Algorithm.GetMosaic(filteredImage, extentSettings.ElementExtent, algorithmSettings.AllowedColors);
 
 				for (int y = 0; y < elementExtent.Height; y++)
 				{
 					for (int x = 0; x < elementExtent.Width; x++)
 					{
-						SKColor color = sourcePixels[source.GetPixelIndex(topLeft.X + x, topLeft.Y + y)];
-						LdColor ldColor = algorithmSettings.Algorithm.GetColor(x, y, color, _colorService);
-						colors[x, y] = ldColor;
-						mosaicPixels[mosaic.GetPixelIndex(x, y)] = ldColor.Color;
+						mosaicPixels[mosaic.GetPixelIndex(x, y)] = mosaicColors[x, y].Color;
 					}
 				}
 
