a16201ca313d8d2820e1a555e6cd02d5eb80e065
David Mackenzie <david@priz.co.uk>
2024-07-30 22:46:59 +0100

    Skip filters when filter properties have no effect

--- src/Chantry.Core/Filters/BrightnessContrastFilter.cs
+++ src/Chantry.Core/Filters/BrightnessContrastFilter.cs
@@ -4,51 +4,54 @@ using TeethInc.Chantry.Core.Extensions;
 
 namespace TeethInc.Chantry.Core.Filters
 {
-    public class BrightnessContrastFilter : Filter
-    {
-        public override string DisplayName => "Brightness and Contrast";
+	public class BrightnessContrastFilter : Filter
+	{
+		public override string DisplayName => "Brightness and Contrast";
 
-        public int Brightness { get; set; }
+		public int Brightness { get; set; }
 
-        public int Contrast { get; set; }
+		public int Contrast { get; set; }
 
-        public TonalRange TonalRange { get; set; } = TonalRange.All;
+		public TonalRange TonalRange { get; set; } = TonalRange.All;
 
-        public BrightnessContrastFilter()
-        {
-            Brightness = 0;
-            Contrast = 0;
-        }
+		public BrightnessContrastFilter()
+		{
+			Brightness = 0;
+			Contrast = 0;
+		}
 
-        protected override void ApplyFilter(SKBitmap image)
-        {
-            image.ApplyFilter(GetAdjustedPixel);
-        }
+		protected override void ApplyFilter(SKBitmap image)
+		{
+			if (Brightness != 0 || Contrast != 0)
+			{
+				image.ApplyFilter(GetAdjustedPixel);
+			}
+		}
 
-        private SKColor GetAdjustedPixel(SKColor unfilteredPixel)
-        {
-            int red = unfilteredPixel.Red;
-            int green = unfilteredPixel.Green;
-            int blue = unfilteredPixel.Blue;
+		private SKColor GetAdjustedPixel(SKColor unfilteredPixel)
+		{
+			int red = unfilteredPixel.Red;
+			int green = unfilteredPixel.Green;
+			int blue = unfilteredPixel.Blue;
 
-            // brightness.
+			// brightness.
 
-            red += Brightness;
-            green += Brightness;
-            blue += Brightness;
+			red += Brightness;
+			green += Brightness;
+			blue += Brightness;
 
-            // contrast.
+			// contrast.
 
-            float factor = 259f * (Contrast + 255f) / (255f * (259f - Contrast));
+			float factor = 259f * (Contrast + 255f) / (255f * (259f - Contrast));
 
-            red = Math.Clamp((int)(factor * (red - 128) + 128), 0, 255);
-            green = Math.Clamp((int)(factor * (green - 128) + 128), 0, 255);
-            blue = Math.Clamp((int)(factor * (blue - 128) + 128), 0, 255);
+			red = Math.Clamp((int)(factor * (red - 128) + 128), 0, 255);
+			green = Math.Clamp((int)(factor * (green - 128) + 128), 0, 255);
+			blue = Math.Clamp((int)(factor * (blue - 128) + 128), 0, 255);
 
-            return new SKColor(
-                (byte)red,
-                (byte)green,
-                (byte)blue);
-        }
-    }
+			return new SKColor(
+				(byte)red,
+				(byte)green,
+				(byte)blue);
+		}
+	}
 }
--- src/Chantry.Core/Filters/SaturationFilter.cs
+++ src/Chantry.Core/Filters/SaturationFilter.cs
@@ -5,51 +5,54 @@ using TeethInc.Chantry.Core.Helpers;
 
 namespace TeethInc.Chantry.Core.Filters
 {
-    public class SaturationFilter : Filter
-    {
-        public override string DisplayName => "Saturation";
+	public class SaturationFilter : Filter
+	{
+		public override string DisplayName => "Saturation";
 
-        public double Saturation { get; set; }
+		public double Saturation { get; set; }
 
-        private byte[] _red;
-        private byte[] _green;
-        private byte[] _blue;
+		private byte[] _red;
+		private byte[] _green;
+		private byte[] _blue;
 
-        public SaturationFilter() : base()
-        {
-            Saturation = 1d;
+		public SaturationFilter() : base()
+		{
+			Saturation = 1d;
 
-            _red = GetCalculatedArray(0.21f);
-            _green = GetCalculatedArray(0.71f);
-            _blue = GetCalculatedArray(0.07f);
-        }
+			_red = GetCalculatedArray(0.21f);
+			_green = GetCalculatedArray(0.71f);
+			_blue = GetCalculatedArray(0.07f);
+		}
 
-        private byte[] GetCalculatedArray(float factor)
-        {
-            var ret = new byte[256];
+		private byte[] GetCalculatedArray(float factor)
+		{
+			var ret = new byte[256];
 
-            for (int i = 0; i < 256; i++)
-            {
-                ret[i] = (byte)(i * factor);
-            }
+			for (int i = 0; i < 256; i++)
+			{
+				ret[i] = (byte)(i * factor);
+			}
 
-            return ret;
-        }
+			return ret;
+		}
 
-        protected override void ApplyFilter(SKBitmap image)
-        {
-            image.ApplyFilter(GetSaturationPixel);
-        }
+		protected override void ApplyFilter(SKBitmap image)
+		{
+			if (Saturation != 1)
+			{
+				image.ApplyFilter(GetSaturationPixel);
+			}
+		}
 
-        private SKColor GetSaturationPixel(SKColor unfilteredPixel)
-        {
-            byte luminosity = (byte)(_red[unfilteredPixel.Red] + _green[unfilteredPixel.Green] + _blue[unfilteredPixel.Blue]);
+		private SKColor GetSaturationPixel(SKColor unfilteredPixel)
+		{
+			byte luminosity = (byte)(_red[unfilteredPixel.Red] + _green[unfilteredPixel.Green] + _blue[unfilteredPixel.Blue]);
 
-            byte red = MathHelper.Lerp(luminosity, unfilteredPixel.Red, Saturation);
-            byte green = MathHelper.Lerp(luminosity, unfilteredPixel.Green, Saturation);
-            byte blue = MathHelper.Lerp(luminosity, unfilteredPixel.Blue, Saturation);
+			byte red = MathHelper.Lerp(luminosity, unfilteredPixel.Red, Saturation);
+			byte green = MathHelper.Lerp(luminosity, unfilteredPixel.Green, Saturation);
+			byte blue = MathHelper.Lerp(luminosity, unfilteredPixel.Blue, Saturation);
 
-            return new SKColor(red, green, blue);
-        }
-    }
+			return new SKColor(red, green, blue);
+		}
+	}
 }
